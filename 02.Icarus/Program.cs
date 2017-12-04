aa37dbb607acd73acdfb08c1e51c1854fda2e3ca
Stefan Koev <koev.stefan@gmail.com>
2017-12-05 01:51:01 +0200

    Create 02.Icarus

---
+++ 02.Icarus/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int startIndex = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            int subtract = 1;
+
+            while (input != "Supernova")
+            {
+                string[] commands = input.Split();
+                string direction = commands[0];
+                int steps = int.Parse(commands[1]);
+
+                switch (direction)
+                {
+                    case "left":
+
+                        while (steps-- > 0)
+                        {
+                            if (startIndex == 0)
+                            {
+                                startIndex = numbers.Length - 1;
+                                subtract++;
+                                numbers[startIndex] -= subtract;
+                                continue;
+                            }
+
+                            startIndex--;
+                            numbers[startIndex] -= subtract;
+                        }
+
+                        break;
+
+                    case "right":
+                        while (steps-- > 0)
+                        {
+                            if (startIndex == numbers.Length - 1)
+                            {
+                                startIndex = 0;
+                                subtract++;
+                                numbers[startIndex] -= subtract;
+                                continue;
+                            }
+
+                            startIndex++;
+                            numbers[startIndex] -= subtract;
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
